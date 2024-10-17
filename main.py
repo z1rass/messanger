2147402791c07fbb8ae9190b1bc44ecd190ea78c
Matvii Kolomiiets <kolomiiets@deventer.loc>
2024-10-17 14:54:03 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from prompt_toolkit import Application
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.layout.containers import VSplit, Window
+from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
+from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.application import get_app
+import requests
+
+kb = KeyBindings()
+
+@kb.add('c-q')
+def exit(event):
+    app.exit()
+    if (len(buffer1.text) > 0):
+        requests.post("http://localhost:8000/user/", json={'text' : buffer1.text})
+
+
+@kb.add('c-s')
+def save(event):
+    text = buffer1.text
+
+# Функция для переключения фокуса между окнами
+@kb.add('c-n')
+def switch_focus(event):
+    get_app().layout.focus_next()
+
+buffer1 = Buffer()  # Editable buffer.
+
+root_container = VSplit([
+    # One window that holds the BufferControl with the default buffer on
+    # the left.
+    Window(content=BufferControl(buffer=buffer1))
+])
+
+layout = Layout(root_container)
+
+app = Application(layout=layout, full_screen=True, key_bindings=kb)
+
+app.run()  # You won't be able to Exit this app
---
+++ server.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import sqlite3
+
+# Создаем соединение с базой данных и создаем курсор
+con = sqlite3.connect("main.db")
+c = con.cursor()
+
+# Создаем таблицу notes, если она не существует
+sql = """CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text TEXT NOT NULL);"""
+c.execute(sql)
+
+# Определяем модель данных
+class Note(BaseModel):
+    text: str
+
+# Создаем экземпляр FastAPI
+app = FastAPI()
+
+# Определяем маршрут для отображения заметок
+@app.get("/")
+async def show_notes():
+    return "Hello"
+
+# Определяем маршрут для добавления заметки
+@app.post("/user/")
+async def add_note(note: Note):
+    # Используем правильное имя таблицы и передаем значение в качестве параметра
+    c.execute("INSERT INTO users (text) VALUES (?)", (note.text,))
+    con.commit()
+    return {"message": "Note added successfully"}
